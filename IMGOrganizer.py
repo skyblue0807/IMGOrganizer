9865376ff446ed73c77f54901c4b659321749a68
JYKim <jykim.0807@gmail.com>
2021-07-21 22:30:09 +0900

    change program detail - comment

--- IMGOrganizer.py
+++ IMGOrganizer.py
@@ -6,7 +6,13 @@ import pandas as pd
 #일단 1) 사람에게 경로를 받음 (일단 1개) -이후에 여러개로 확장
 # 2) 보고서를 쓰기 위한 파일명을 생성(일시 + 랜덤수 3개)
 # 3) 폴더 속 파일 리스트, 생성일, 크기를 csv파일로 저장 // https://mentha2.tistory.com/207
+# 확인해보니, 파일 생성일 및 수정일은 사진과 무관.. 
+# 따라서 파일명을 가지고 사진 파일의 YYYYMMDDHHMMSS를 14자리 숫자로 맨 앞에 덧붙여서 이후엔 이걸 기준으로 파일을 분류하고자 함
+# 파일명의 종류
+### 2020-02-22-15-32-06.jpg
+### 20200118_140409.jpg
 # 4) 파일 리스트를 report 폴더 내에 csv로 저장
+# 프로그램 목표를 gif랑 일반 사진 파일, 동영상을 나누는 것으로 해야 할듯
 
 #보고서 이름명 생성
 case_name = dt.datetime.now().strftime("%Y%m%d%H%M%S") + str(random.randrange(0, 10)) + str(random.randrange(0, 10))
